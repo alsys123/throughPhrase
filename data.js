842885e3ed665b384acc694efc4a4498526ec62f
alsys123 <alsys123@gmail.com>
2026-05-28 10:24:27 -0400

    cleanup and some improvements

---
+++ data.js
@@ -0,0 +1,123 @@
+    // Game data - each game has multiple through lines whose answers form a phrase
+    const games = [
+        {
+            phrase: "EVERY CLOUD HAS A SILVER LINING",
+            throughLines: [
+                {
+                    words: ['WHERE', 'BODY', 'DAY', 'THING', 'ONE'],
+                    answer: 'EVERY',
+                    connections: ['EVERYWHERE', 'EVERYBODY', 'EVERYDAY', 'EVERYTHING', 'EVERYONE']
+                },
+                {
+                    words: ['NINE', 'STORAGE', 'COMPUTING', 'COVER', 'FOREST'],
+                    answer: 'CLOUD',
+                    connections: ['CLOUD NINE', 'CLOUD STORAGE', 'CLOUD COMPUTING', 'CLOUD COVER', 'CLOUD FOREST']
+                },
+                {
+                    words: ['BEEN', 'BROWNS', 'TAG', 'ANYONE', 'SEEN'],
+                    answer: 'HAS',
+                    connections: ['HAS BEEN', 'HASH BROWNS', 'HASH TAG', 'HAS ANYONE', 'HAS SEEN']
+                },
+                {
+                    words: ['SPOON', 'SCREEN', 'BULLET', 'LINING', 'MEDAL'],
+                    answer: 'SILVER',
+                    connections: ['SILVER SPOON', 'SILVER SCREEN', 'SILVER BULLET', 'SILVER LINING', 'SILVER MEDAL']
+                },
+                {
+                    words: ['UP', 'DANCE', 'ITEM', 'BACKER', 'DRIVE'],
+                    answer: 'LINE',
+                    connections: ['LINE UP', 'LINE DANCE', 'LINE ITEM', 'LINE BACKER', 'LINE DRIVE']
+                }
+            ]
+        },
+        {
+            phrase: "TIME FLIES WHEN HAVING FUN",
+            throughLines: [
+                {
+                    words: ['KEEPER', 'ZONE', 'BOMB', 'PIECE', 'SHARE'],
+                    answer: 'TIME',
+                    connections: ['TIMEKEEPER', 'TIME ZONE', 'TIME BOMB', 'TIMEPIECE', 'TIMESHARE']
+                },
+                {
+                    words: ['BALL', 'PAPER', 'SWATTER', 'WHEEL', 'FISHING'],
+                    answer: 'FLY',
+                    connections: ['FLY BALL', 'FLY PAPER', 'FLY SWATTER', 'FLY WHEEL', 'FLY FISHING']
+                },
+                {
+                    words: ['EVER', 'HARRY', 'MET', 'SALLY', 'NEEDED'],
+                    answer: 'WHEN',
+                    connections: ['WHENEVER', 'WHEN HARRY', 'WHEN MET', 'WHEN SALLY', 'WHEN NEEDED']
+                },
+                {
+                    words: ['FUN', 'DINNER', 'TROUBLE', 'SECOND', 'THOUGHTS'],
+                    answer: 'HAVING',
+                    connections: ['HAVING FUN', 'HAVING DINNER', 'HAVING TROUBLE', 'HAVING SECOND', 'HAVING THOUGHTS']
+                }
+            ]
+        },
+        {
+            phrase: "BREAK THE ICE",
+            throughLines: [
+                {
+                    words: ['FAST', 'THROUGH', 'DOWN', 'EVEN', 'NECK'],
+                    answer: 'BREAK',
+                    connections: ['BREAKFAST', 'BREAKTHROUGH', 'BREAKDOWN', 'BREAK EVEN', 'BREAKNECK']
+                },
+                {
+                    words: ['END', 'BEGINNING', 'BEST', 'WORST', 'REASON'],
+                    answer: 'THE',
+                    connections: ['THE END', 'THE BEGINNING', 'THE BEST', 'THE WORST', 'THE REASON']
+                },
+                {
+                    words: ['CREAM', 'HOCKEY', 'CUBE', 'BREAKER', 'AGE'],
+                    answer: 'ICE',
+                    connections: ['ICE CREAM', 'ICE HOCKEY', 'ICE CUBE', 'ICEBREAKER', 'ICE AGE']
+                }
+            ]
+        },
+        {
+            phrase: "HOME SWEET HOME",
+            throughLines: [
+                {
+                    words: ['WORK', 'TOWN', 'MADE', 'COMING', 'RUN'],
+                    answer: 'HOME',
+                    connections: ['HOMEWORK', 'HOMETOWN', 'HOMEMADE', 'HOMECOMING', 'HOME RUN']
+                },
+                {
+                    words: ['SIXTEEN', 'TOOTH', 'HEART', 'DREAMS', 'SPOT'],
+                    answer: 'SWEET',
+                    connections: ['SWEET SIXTEEN', 'SWEET TOOTH', 'SWEETHEART', 'SWEET DREAMS', 'SWEET SPOT']
+                }
+            ]
+        },
+        {
+            phrase: "ONCE IN A BLUE MOON",
+            throughLines: [
+                {
+                    words: ['UPON', 'MORE', 'AGAIN', 'OVER', 'BITTEN'],
+                    answer: 'ONCE',
+                    connections: ['ONCE UPON', 'ONCE MORE', 'ONCE AGAIN', 'ONCE OVER', 'ONCE BITTEN']
+                },
+                {
+                    words: ['CROWD', 'LOVE', 'TRANSIT', 'TOUCH', 'FLUX'],
+                    answer: 'IN',
+                    connections: ['IN CROWD', 'IN LOVE', 'IN TRANSIT', 'IN TOUCH', 'IN FLUX']
+                },
+                {
+                    words: ['MINOR', 'MAJOR', 'FLAT', 'SHARP', 'TEAM'],
+                    answer: 'A',
+                    connections: ['A MINOR', 'A MAJOR', 'A FLAT', 'A SHARP', 'A TEAM']
+                },
+                {
+                    words: ['COLLAR', 'RIBBON', 'SCREEN', 'BLOOD', 'CHIP'],
+                    answer: 'BLUE',
+                    connections: ['BLUE COLLAR', 'BLUE RIBBON', 'BLUE SCREEN', 'BLUE BLOOD', 'BLUE CHIP']
+                },
+                {
+                    words: ['LIGHT', 'WALK', 'SHINE', 'BEAM', 'LANDING'],
+                    answer: 'MOON',
+                    connections: ['MOONLIGHT', 'MOONWALK', 'MOONSHINE', 'MOON BEAM', 'MOON LANDING']
+                }
+            ]
+        }
+    ];
